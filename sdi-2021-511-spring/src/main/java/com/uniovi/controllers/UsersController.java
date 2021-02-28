6de58eaf1f6d8532d9f83e380bc6e264eab226f5
Alba <uo266007@uniovi.es>
2021-02-28 23:35:07 +0100

    SDI-2021-511-4.2-Roles

--- sdi-2021-511-spring/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi-2021-511-spring/src/main/java/com/uniovi/controllers/UsersController.java
@@ -1,3 +1,4 @@
+
 package com.uniovi.controllers;
 
 import org.springframework.beans.factory.annotation.*;
@@ -9,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.entities.*;
+import com.uniovi.service.RolesService;
 import com.uniovi.service.SecurityService;
 import com.uniovi.service.UsersService;
 import com.uniovi.validators.SignUpFormValidator;
@@ -23,6 +25,9 @@ public class UsersController {
 
 	@Autowired
 	private SignUpFormValidator signUpFormValidator;
+	
+	@Autowired
+	private RolesService rolesService;
 
 	@RequestMapping("/user/list")
 	public String getListado(Model model) {
@@ -36,7 +41,8 @@ public class UsersController {
 	}
 	@RequestMapping(value = "/user/add")
 	public String getUser(Model model) {
-		model.addAttribute("usersList", usersService.getUsers());
+		model.addAttribute("rolesList", rolesService.getRoles());
+		System.err.println(model.getAttribute("HOLA"+"rolesList"));
 		return "user/add";
 	}
 
@@ -51,6 +57,8 @@ public class UsersController {
 		model.addAttribute("user", usersService.getUser(id));
 		return "user/details";
 	}
+	
+	
 
 	@RequestMapping("/user/delete/{id}")
 	public String delete(@PathVariable Long id) {
@@ -64,11 +72,7 @@ public class UsersController {
 		model.addAttribute("user", user);
 		return "user/edit";
 	}
-	@RequestMapping(value = "/user/add", method = RequestMethod.GET)
-	public String setMark(Model model) {
-	model.addAttribute("user", new User());
-	 return "user/add";
-	}
+	
 	@RequestMapping(value = "/user/edit/{id}", method = RequestMethod.POST)
 	public String setEdit(Model model, @PathVariable Long id, @ModelAttribute User user) {
 	//	user.setId(id);
@@ -87,16 +91,16 @@ public class UsersController {
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public String signup(@Validated User user, BindingResult result) {
-
-		signUpFormValidator.validate(user, result);
-		if (result.hasErrors()) {
-
-			return "signup";
-		}
-		usersService.addUser(user);
-		securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
-		return "redirect:home";
+	 signUpFormValidator.validate(user, result);
+	 if (result.hasErrors()) {
+	 return "signup";
+	 }
+	 user.setRole(rolesService.getRoles()[0]);
+	 usersService.addUser(user);
+	 securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
+	 return "redirect:home";
 	}
+
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model) {
@@ -116,3 +120,4 @@ public class UsersController {
 
 
 }
+
